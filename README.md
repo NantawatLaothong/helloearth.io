5db9f14c185b8cb589f8b976a35303b06a8dc5d2
Nantawat Laothong <nantawat.laothong001@umb.edu>
2022-01-10 01:18:11 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Wordpress Custom theme
+
+A personal theme for my portfolio website that will be used for helloearth.io
+
---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php 
+    wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri(  ) . '/css/bootstrap.min.css');
+    wp_enqueue_style('style', get_stylesheet_uri(  ));
+?>
